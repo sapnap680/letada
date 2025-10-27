4e32e797e3a041df3b80fe2d0d9d7d151bdfcc19
sapnap680 <02057478@keio.jp>
2025-10-28 01:02:04 +0900

    Delete ddadam directory

--- ddadam/frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // env セクションは削除
-  // Next.js は NEXT_PUBLIC_ プレフィックスの環境変数を自動的に公開します
-}
-
-module.exports = nextConfig
-
